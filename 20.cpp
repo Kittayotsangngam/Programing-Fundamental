b60b99031203be58b0e8561ba32fe78bb58196d9
Kittayotsangngam <88589709+Kittayotsangngam@users.noreply.github.com>
2021-08-12 20:38:34 +0700

    Add files via upload

---
+++ 20.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int row, no;
+scanf("%d", &no);
+row=no;
+while( row>0 ){
+		printf("*");
+        row--;
+	}
+return 0;
+}
---
+++ PF_20.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ int row, no;
+scanf("%d", &no);
+for( row =0; row< no; row++){
+		printf("*");
+	}
+return 0;
+}
