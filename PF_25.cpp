0b622cfd81819264e624fc2c7ff7cce1b6aad1fb
Kittayotsangngam <88589709+Kittayotsangngam@users.noreply.github.com>
2021-08-27 20:47:30 +0700

    Update PF_25.cpp

--- PF_25.cpp
+++ PF_25.cpp
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a;
+    int a,k=0;
     scanf("%d", &a);
-    for(int i=0 ;i<=a;i++)
-    {
-        for(int j=1;j<=a-i;j++)
-        printf(" ");
-
-        for(int k=1;k<=(2*i-1);k++)
-        printf("*");
-
+    for(int i=1 ;i<=a;++i)
+    {   k=0;
+    for(int j=1;j<=a-i;++j)
+        {printf(" ");
+        }
+        while(k!=(2*i-1))
+        {printf("*");
+        ++k;
+        }
         printf("\n");
-
     }
 }
