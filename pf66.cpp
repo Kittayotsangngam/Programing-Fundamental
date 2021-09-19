630a8057024902ebe47076659ab56f3cbdc1e7a6
Kittayotsangngam <88589709+Kittayotsangngam@users.noreply.github.com>
2021-09-19 20:42:32 +0700

    Add files via upload

---
+++ pf66.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{double x,y;
+    scanf("%lf",&x);
+    scanf("%lf",&y);
+    printf("before swap\n");
+    printf("%lf\n", x);
+    printf("%lf\n", y);
+   x = (x + y) - (y = x);
+    printf("after swap\n");
+    printf("%lf\n", x);
+    printf("%lf\n", y);
+}
